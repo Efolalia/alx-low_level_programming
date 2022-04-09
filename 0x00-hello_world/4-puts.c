afef7e67a175f03b7c4cc1287fb4fb051cab4b8e
Fatou <fbtouray123@gmail.com>
2022-04-08 17:38:33 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Function begins
+*main - function begins
 *
-* Return: Function ends
+* Return: function ends
 */
+
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
